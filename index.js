c5b3999f1aec71535c0009eed4e042fe2775cf5d
nicoly <nicolypinheiros@gmail.com>
2020-02-11 20:54:17 -0300

    adicionou raiz quadrada

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const button4 = document.querySelector("#button4");
+const button5 = document.querySelector("#button5")
 button1.onclick = function() {
  console.log( +input1.value + +input2.value)
   inputResultado.value = ( +input1.value + +input2.value);
@@ -21,5 +22,9 @@ button2.onclick = function() {
  button4.onclick = function() {
   console.log( +input1.value * +input2.value)
    inputResultado.value = ( +input1.value * +input2.value);
+ };
+ button5.onclick = function() {
+  console.log(Math.sqrt (input1.value) )
+   inputResultado.value = Math.sqrt(input1.value)
  };
    
