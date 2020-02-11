6be32f0c93e7445fb8fcc6c8d2529da850bc6ef8
nicoly <nicolypinheiros@gmail.com>
2020-02-11 20:42:01 -0300

    editamos a calculadora

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const input1 = document.querySelector("input#input1")
 const input2 = document.querySelector("input#input2")
 const inputResultado = document.querySelector("#inputResultado")
 
-const button = document.querySelector("button");
-button.onclick = function() {
-  const p = document.querySelector("p");
-  console.log(p);
-  p.innerHTML = new Date();
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+button1.onclick = function() {
+ console.log( +input1.value + +input2.value)
+  inputResultado.value = ( +input1.value + +input2.value);
 };
+button2.onclick = function() {
+  console.log( +input1.value - +input2.value)
+   inputResultado.value = ( +input1.value - +input2.value);
+ };
+ button3.onclick = function() {
+  console.log( +input1.value / +input2.value)
+   inputResultado.value = ( +input1.value / +input2.value);
+ };
+ button4.onclick = function() {
+  console.log( +input1.value * +input2.value)
+   inputResultado.value = ( +input1.value * +input2.value);
+ };
+   
